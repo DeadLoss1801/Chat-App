2cde4ced8b35dff5f5d80730d52012778596c096
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2023-04-18 20:33:41 +0530

    This adding the files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+// import Register from "./Pages/Register"
+import "./Pages/style.scss"
+// import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
